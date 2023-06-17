4d394b8225695a7f9803f93b58b5586e83ed3a4e
Bhavesh Yadav <codezera3@gmail.com>
2023-06-17 09:37:54 +0530

    initial commit

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: date,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('user', userSchema);
